e0eca0e611fece5e3b6a74bc39a69115deae34ee
chndrwali <chndrwali@gmail.com>
2024-05-17 18:44:43 +0700

    add form ucapan doa

--- components/pages/doa-ucapan.tsx
+++ components/pages/doa-ucapan.tsx
@@ -16,35 +16,42 @@ const DoaUcapan = () => {
     <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 py-12">
       <div className="absolute inset-0 bg-gradient-to-b from-white to-gray-100 opacity-90"></div>
       <div className="relative z-10 text-center mb-12">
-        <h1 className="text-5xl font-bold">Doa & Ucapan untuk Pasangan Mempelai</h1>
-        <p className="mt-4 text-lg text-gray-700">Merupakan suatu kehormatan dan kebahagiaan bagi kami, apabila Bapak/Ibu/Saudara/i berkenan hadir, untuk memberikan doa restu kepada kedua mempelai.</p>
+        <h1 className="text-xl font-bold">
+          Doa & Ucapan untuk <br />
+          <span className="text-4xl font-black">Pasangan Mempelai</span>{' '}
+        </h1>
+        <div className="max-w-xl">
+          <p className="mt-4 text-lg text-gray-700">Merupakan suatu kehormatan dan kebahagiaan bagi kami, apabila Bapak/Ibu/Saudara/i berkenan hadir, untuk memberikan doa restu kepada kedua mempelai.</p>
+        </div>
       </div>
-      <div className="relative z-10 w-full max-w-3xl bg-white p-8 rounded-lg shadow-lg">
-        <form className="mb-8">
-          <div className="mb-4">
-            <input type="text" placeholder="Nama Anda" className="w-full p-3 border border-gray-300 rounded-lg" />
-          </div>
-          <div className="mb-4">
-            <textarea placeholder="Berikan Ucapan Dan Doa Restu" className="w-full p-3 border border-gray-300 rounded-lg" rows={4}></textarea>
-          </div>
-          <div className="mb-4">
-            <select className="w-full p-3 border border-gray-300 rounded-lg">
-              <option value="">Konfirmasi Kehadiran</option>
-              <option value="yes">Ya</option>
-              <option value="no">Tidak</option>
-            </select>
-          </div>
-          <button type="submit" className="w-full bg-brown-500 text-white p-3 rounded-lg">
-            Kirim
-          </button>
-        </form>
-        <div className="space-y-6">
-          {comments.map((comment, index) => (
-            <div key={index} className="p-4 bg-gray-50 border border-gray-200 rounded-lg shadow-sm">
-              <h2 className="text-lg font-bold text-gray-900">{comment.name}</h2>
-              <p className="text-gray-700">{comment.message}</p>
+      <div className="bg-black rounded-xl p-8">
+        <div className="relative z-10 w-full max-w-xl sm:max-w-3xl bg-white p-8 rounded-xl shadow-lg">
+          <form className="mb-8">
+            <div className="mb-4">
+              <input type="text" placeholder="Nama Anda" className="w-full p-3 border border-gray-300 rounded-lg" />
             </div>
-          ))}
+            <div className="mb-4">
+              <textarea placeholder="Berikan Ucapan Dan Doa Restu" className="w-full p-3 border border-gray-300 rounded-lg" rows={4}></textarea>
+            </div>
+            <div className="mb-4">
+              <select className="w-full p-3 border border-gray-300 rounded-lg">
+                <option value="">Konfirmasi Kehadiran</option>
+                <option value="yes">Ya</option>
+                <option value="no">Tidak</option>
+              </select>
+            </div>
+            <button type="submit" className="w-full bg-brown-500 text-white p-3 rounded-lg">
+              Kirim
+            </button>
+          </form>
+          <div className="space-y-6">
+            {comments.map((comment, index) => (
+              <div key={index} className="p-4 bg-gray-50 border border-gray-200 rounded-lg shadow-sm">
+                <h2 className="text-lg font-bold text-gray-900">{comment.name}</h2>
+                <p className="text-gray-700">{comment.message}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- components/pages/quotes-rezeki.tsx
+++ components/pages/quotes-rezeki.tsx
@@ -1,7 +1,7 @@
 const QuoteSection = () => {
   return (
     <div className="relative w-full h-screen bg-gray-900 flex items-center justify-center">
-      <div className="relative z-10 text-center text-white px-4">
+      <div className="relative z-10 text-center text-white px-4 max-w-xl">
         <p className="text-lg font-semibold">
           {`"`}Semoga Allah memberkahimu dan memberkahi apa yang menjadi tanggung jawabmu, serta menyatukan kalian berdua dalam kebaikan.{`"`}
         </p>
